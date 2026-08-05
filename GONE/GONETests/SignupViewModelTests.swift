e678bd3deab75586b8a60d764d5c217c5d827bf7
kimeunchan <kec4489@naver.com>
2026-08-05 16:56:51 +0900

    feat: 회원가입 학생 정보 및 프로필 사진 추가 (#10)

--- GONE/GONETests/SignupViewModelTests.swift
+++ GONE/GONETests/SignupViewModelTests.swift
@@ -51,4 +51,56 @@ final class SignupViewModelTests: XCTestCase {
         XCTAssertEqual(viewModel.phoneNumber, "010-1234-5678")
         XCTAssertTrue(viewModel.isVerificationRequestEnabled)
     }
+
+    func testStudentInformationStepRequiresBothStudentNumberAndName() {
+        let viewModel = makeViewModelAtStudentInformationStep()
+
+        XCTAssertFalse(viewModel.isPrimaryActionEnabled)
+
+        viewModel.studentNumber = "3206"
+        XCTAssertFalse(viewModel.isPrimaryActionEnabled)
+
+        viewModel.proceed()
+        XCTAssertEqual(viewModel.currentStep, .studentInformation)
+        XCTAssertNil(viewModel.studentNumberErrorMessage)
+        XCTAssertEqual(viewModel.nameErrorMessage, "이름을 입력해주세요.")
+
+        viewModel.name = "김은찬"
+        XCTAssertTrue(viewModel.isPrimaryActionEnabled)
+    }
+
+    func testStudentInformationStepMovesToOptionalProfileImageStep() {
+        let viewModel = makeViewModelAtStudentInformationStep()
+        viewModel.studentNumber = "3206"
+        viewModel.name = "김은찬"
+
+        viewModel.proceed()
+
+        XCTAssertEqual(viewModel.currentStep, .profileImage)
+        XCTAssertEqual(viewModel.currentStep.actionTitle, "시작하기")
+        XCTAssertTrue(viewModel.isPrimaryActionEnabled)
+    }
+
+    func testProfileImageDataIsStoredWhenSelected() {
+        let viewModel = SignupViewModel()
+        let imageData = Data([0x01, 0x02])
+
+        viewModel.updateProfileImage(data: imageData)
+
+        XCTAssertEqual(viewModel.profileImageData, imageData)
+        XCTAssertNil(viewModel.profileImageErrorMessage)
+    }
+
+    private func makeViewModelAtStudentInformationStep() -> SignupViewModel {
+        let viewModel = SignupViewModel()
+        viewModel.identifier = "goneUser"
+        viewModel.proceed()
+        viewModel.password = "password"
+        viewModel.passwordConfirmation = "password"
+        viewModel.proceed()
+        viewModel.updatePhoneNumber("01012345678")
+        viewModel.verificationCode = "123456"
+        viewModel.proceed()
+        return viewModel
+    }
 }
